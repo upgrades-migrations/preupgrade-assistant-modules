357458dc30e38af3a28b05c83d1f25ff30f6459e
Jiri Hornicek <jhornice@redhat.com>
2016-07-01 15:38:12 +0200

    lang check of LoadBalanceSupport

--- RHEL6_7/packages/LoadBalanceSupport/check.sh
+++ RHEL6_7/packages/LoadBalanceSupport/check.sh
@@ -16,35 +16,35 @@ declare -a new_lb_check=$(get_dist_native_list | egrep '^(keepalived|haproxy|pir
 
 if [[ "${new_lb_check[@]}" =~ "ipvsadm" ]];then
   if [[ "${new_lb_check[@]}" =~ "piranha" ]];then
-     echo "You have installed piranha package on your system. This is no longer supported load balancer 
-solution. For RHEl 7 compatible load balancer support install keepalived and haproxy packages" >> $SOLUTION_FILE
-     log_high_risk "For RHEl 7 compatible load balancer support run 'yum install keepalived haproxy'"
+     echo "You have installed a piranha package on your system. This is a no longer supported load balancer 
+solution. Install keepalived and haproxy packages for Red Hat Enterprise Linux 7 compatible load balancer support." >> $SOLUTION_FILE
+     log_high_risk "Run 'yum install keepalived haproxy' for Red Hat Enterprise Linux 7 compatible load balancer support."
      exit_fail
   else
      if [[ "${new_lb_check[@]}" =~ "keepalived" ]] && [[ "${new_lb_check[@]}" =~ "haproxy" ]];then
-       echo "Load balancer support on this system is fully compatible with RHEL 7" >> $SOLUTION_FILE
+       echo "Load balancer support on this system is fully compatible with Red Hat Enterprise Linux 7." >> $SOLUTION_FILE
        exit_pass
      elif [[ "${new_lb_check[@]}" =~ "keepalived" ]] ;then
-       echo "Your system has full support for RHEL 7 compatible LVS based load balancer. For tcp/http based load balancer and proxy install haproxy package " >> $SOLUTION_FILE
-       log_none_risk "For tcp/http based load balancer and proxy run 'yum install haproxy'"
+       echo "Your system has full support for Red Hat Enterprise Linux 7 compatible LVS based load balancer. For tcp/http based load balancer and proxy install haproxy package." >> $SOLUTION_FILE
+       log_none_risk "For tcp/http based load balancer and proxy run 'yum install haproxy'."
        exit_informational
      elif [[ "${new_lb_check[@]}" =~ "haproxy" ]] ;then
-       echo "You have installed haproxy tcp/http based load balancer and proxy. You have installed ipvsadm LVS based load balancer,if you want to implement additional layer for health check and failover handling, install keepalived package" >> $SOLUTION_FILE
-       log_none_risk "For for health check and failover handling of your LVS based load balancer run 'yum install keepalived'"
+       echo "You have installed haproxy tcp/http based load balancer and proxy. You have installed ipvsadm LVS based load balancer. If you want to implement an additional layer for a health check and failover handling, install a keepalived package" >> $SOLUTION_FILE
+       log_none_risk "For a health check and failover handling of your LVS based load balancer run 'yum install keepalived'."
        exit_informational
      else
-       echo "You have installed ipvsadm LVS based load balancer,if you want to implement additional layer for health check and failover handling, install keepalived package. You can install haproxy package for tcp/http based load balancer and proxy" >> $SOLUTION_FILE
-       log_none_risk "For health check and failover handling run 'yum install keepalived' for tcp/http based load balancer and proxy 'yum install haproxy'"
+       echo "You have installed ipvsadm LVS based load balancer. If you want to implement an additional layer for a health check and failover handling, install a keepalived package. You can install a haproxy package for tcp/http based load balancer and proxy" >> $SOLUTION_FILE
+       log_none_risk "For a health check and failover handling run 'yum install keepalived'. For tcp/http based load balancer and proxy run 'yum install haproxy'."
        exit_informational
      fi
   fi
 elif [[ "${new_lb_check[@]}" =~ "haproxy" ]] ;then
     if [[ "${new_lb_check[@]}" =~ "keepalived" ]] ;then
-  echo "You have installed haproxy tcp/http based load balancer and proxy. If you want to implement also LVS based load balancer, install ipvsadm package" >> $SOLUTION_FILE  
+  echo "You have installed haproxy tcp/http based load balancer and proxy. If you also want to implement LVS based load balancer, install an ipvsadm package" >> $SOLUTION_FILE  
   log_none_risk "For LVS based Load Balancer run 'yum install ipvsadm'"
   exit_informational
     else
-  echo "You have installed haproxy tcp/http based load balancer and proxy. If you want to implement also LVS based load balancer, install ipvsadm and keepalived packages" >> $SOLUTION_FILE  
+  echo "You have installed haproxy tcp/http based load balancer and proxy. If you also want to implement LVS based load balancer, install ipvsadm and keepalived packages" >> $SOLUTION_FILE  
   log_none_risk "For LVS based Load Balancer run 'yum install ipvsadm keepalived'"
   exit_informational
   fi
