f7e1a18da48b8e6a4740749066f3dbc0c0f3a9ed
Petr Stodulka <pstodulk@redhat.com>
2016-07-21 18:16:03 +0200

    packages/NonRHSignedPkg (#1358847, #1308582)
    - Use new API to get current set of non-native packages
    - Use new variables from PA API for correct paths of files
    - PASS when all packages are native - previously there was exit pass
      included in subshell, so exit code was processed by the module
      instead of PA. Now it is just in curly brackkets as block.

--- RHEL6_7/packages/NonRHSignedPkg/nonrhpkg.sh
+++ RHEL6_7/packages/NonRHSignedPkg/nonrhpkg.sh
@@ -11,11 +11,20 @@ COMPONENT="distribution"
 
 #check for Red Hat RHEL 6 keys and filter them out.
 #based on https://access.redhat.com/site/security/team/key/
-grep -v "199e2f91fd431d51" "$VALUE_RPM_QA" | grep -v "5326810137017186" | \
-grep -v "938a80caf21541eb" | grep -v "fd372689897da07a" | \
-grep -v "45689c882fa658e0" >"$VALUE_TMP_PREUPGRADE/kickstart/nonrhpkgs" || \
-(log_info "All packages are RH signed, no 3rd party keys detected" && exit $RESULT_PASS)
+get_dist_non_native_list() {
+  local pkg
+  while read line; do
+    pkg=$(echo $line | cut -d " " -f1 )
+    is_dist_native $pkg >/dev/null || echo $pkg
+  done < "$VALUE_RPM_QA"
+}
+
 echo " * nonrhpkgs - this file contains all RHEL 6 packages not signed by RH keys - you will have to handle them yourself." >>"$KICKSTART_README"
+get_dist_non_native_list > "$KICKSTART_DIR/nonrhpkgs"
+[ -s "$KICKSTART_DIR/nonrhpkgs" ] || {
+  log_info "All packages are RH signed, no 3rd party keys detected"
+  exit $RESULT_PASS;
+}
 
 #We detected some non-redhat package
 log_high_risk "We detected some non-RH signed packages, you can find the list in [link:kickstart/nonrhpkgs]. You need to handle them yourself!"
