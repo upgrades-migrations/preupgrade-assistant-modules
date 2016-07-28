118df7ae42d4f30a60a7a7c7ae7d0741df63d728
Jiri Hornicek <jhornice@redhat.com>
2016-07-28 09:58:20 +0200

    language check - system/Architecture (#1240746)

--- RHEL6_7/system/Architecture/architecture.sh
+++ RHEL6_7/system/Architecture/architecture.sh
@@ -12,7 +12,7 @@ myarch=`arch`
 if [ $myarch == "i686" ] || [ $myarch == "ppc" ] || [ $myarch == "s390" ];
 then
   lscpu | grep "64-bit" >/dev/null && log_extreme_risk "Your system has 64 bit capabilities, but the installation is 32 bit. This may very likely break the in-place upgrade. You should consider clean installation." && exit $RESULT_FAIL
-  log_extreme_risk "Your system has only 32 bit capabilities. RHEL 7 32 bit installations are not supported. You should consider keeping RHEL 6 or new hardware/CPU."
+  log_extreme_risk "Your system has only 32 bit capabilities. Red Hat Enterprise Linux 7 32 bit installations are not supported. You should consider keeping Red Hat Enterprise Linux 6 or getting new hardware/CPU."
 fi
 if [ $myarch == "ppc64" ] || [ $myarch == "x86_64" ] || [ $myarch == "s390x" ];
 then
