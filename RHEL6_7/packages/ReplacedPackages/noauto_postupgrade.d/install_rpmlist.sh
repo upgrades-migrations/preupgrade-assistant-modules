9dcf3568ca3000e4dfbddba6db9d9d230333524f
Petr Stodulka <pstodulk@redhat.com>
2017-05-03 19:55:34 +0200

    packages/ReplacedPackages: for install_rpmlist.sh (#1414556)
    The install_rpmlist.sh script was broken because the format
    of input data has been changed. Now input data are parsed correctly.
    
    In addition, the script was really ineffective as it installed just
    small sets of packages in each transactions (usually just one
    package per transaction). Now it istalls all packages designated
    to be installed at once. Logs has been improved as well.

--- RHEL6_7/packages/ReplacedPackages/noauto_postupgrade.d/install_rpmlist.sh
+++ RHEL6_7/packages/ReplacedPackages/noauto_postupgrade.d/install_rpmlist.sh
@@ -15,14 +15,29 @@
   exit 2
 }
 
-fail=0
+get_pkgs() {
+  grep -v "^[[:space:]]*#" "$1" \
+    | cut -d "|" -f3 \
+    | tr "," " " \
+    | tr "\n" " "
+}
 
-while read line; do
-  pkgs="$(echo $line | cut -d " " -f1 | tr "," " ")"
-  yum install -y $pkgs || {
-    echo "Error: The packages [$pkgs] were not installed." >&2
-    fail=1
-  }
-done < "$1"
+pkgs="$( get_pkgs "$1" )"
+yum install -y --skip-broken $pkgs || {
+  echo "Error: Packages have not been installed" >&2
+  exit 1
+}
 
-exit $fail
+missing_pkgs=""
+for p in $pkgs; do
+  rpm -q $p >/dev/null 2>/dev/null || missing_pkgs+="\n    $p"
+done
+
+[ -n "$missing_pkgs" ] && {
+  echo -e "Error: Packages below have not been installed:$missing_pkgs" >&2
+  exit 1
+}
+
+echo "Info: all packages have been installed successfully." >&2
+exit 0
+
