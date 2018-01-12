a3fdc20252c81712631ae380147d278dfc3c7715
Petr Stodulka <pstodulk@redhat.com>
2018-01-12 18:35:31 +0100

    openssh/sshd: Fix logic of the module
    Brief description of changes:
     (check)
      - the sshd_config file is stored under cleanconf only when result
        is fixed and user do not have to do anything
      - when RequiredAuthentications1 is detected, slight risk is logged
        and the modified config will be stored under dirtyconf
      - in case any expected modification fail, user is required to
        prepare the config manually under cleanconf
      - modified texts - it should be more relevant now
     (postugrade)
      - removed check of sysconfig (done by different module)
      - ends when cleanconf is detected and do nothing,
      - otherwise back up original conf with ".preupg_bp" suffix
      - when dirtyconf is detected, copy the dirtyconf config to
        /etc/ssh/sshd/config
      - in case that dirtyconf && cleanconf version of sshd_config file
        is missing, log error message and try to rescue situation with
        current sshd_config (it can fail in case it failed before and
        user didn't fixed the file manually)
    
    NOTE:
    Checking & modifications during postupgrade phase are kept as before
    just as insurance in case something will go terribly wrong. However,
    it is expected that checks do not find anything as everything should
    be already fixed.
    
    Texts reviewed and modified by jhornice@redhat.com

--- RHEL6_7/services/openssh/sshd/postupgrade.d/openssh.sh
+++ RHEL6_7/services/openssh/sshd/postupgrade.d/openssh.sh
@@ -1,20 +1,37 @@
 #!/bin/bash
 
-CONFIG_FILE=/etc/sysconfig/sshd
+log_info() {
+  echo >&2 "Info: $@"
+}
 
-if [ ! -f "$CONFIG_FILE" ]; then
-    echo "The configuration file does not exist."
+log_error() {
+  echo >&2 "Error: $@"
+}
+
+CONFIG_FILE=/etc/ssh/sshd_config
+DIRTY_CONFIG_FILE=/root/preupgrade/dirtyconf/etc/ssh/sshd_config
+CLEAN_CONFIG_FILE=/root/preupgrade/cleanconf/etc/ssh/sshd_config
+
+if [ -f "$CLEAN_CONFIG_FILE" ]; then
+    log_info "The $CLEAN_CONFIG_FILE file was detected. No activity needed."
     exit 0
 fi
 
-echo "Check whether exports are available in the $CONFIG_FILE file."
-grep "^export" $CONFIG_FILE
-if [ $? -eq 0 ]; then
-    sed -i -e 's/^export //' $CONFIG_FILE
+log_info "Back up the $CONFIG_FILE file as the ${CONFIG_FILE}.preupg_bp."
+cp -a "${CONFIG_FILE}" "${CONFIG_FILE}.preupg_bp"
+
+if [ ! -f "$DIRTY_CONFIG_FILE" ]; then
+    msg="Neither the $DIRTY_CONFIG_FILE file or the $CLEAN_CONFIG_FILE file have been"
+    msg+=" detected. Try to fix the current $CONFIG_FILE config file."
+    log_error "$msg"
+else
+    log_info "Copy the $DIRTY_CONFIG_FILE file to the $CONFIG_FILE file."
+    cp -a "$DIRTY_CONFIG_FILE" "${CONFIG_FILE}"
 fi
 
-CONFIG_FILE=/etc/ssh/sshd_config
 
+### Code below is kept here just as insurance. It is expected that file will be
+### already fixed correctly (automatically or manually by user).
 grep "^[[:space:]]*RequiredAuthentications2" $CONFIG_FILE
 if [ $? -eq 0 ]; then
     sed -i -e 's/^\([[:space:]]*\)RequiredAuthentications2/\1AuthenticationMethods/' $CONFIG_FILE
@@ -27,7 +44,7 @@ fi
 
 if grep -q -i "^[[:space:]]*AuthorizedKeysCommand[[:space:]]" $CONFIG_FILE; then
     if grep -q -i "^[[:space:]]*AuthorizedKeysCommandRunAs[[:space:]]" $CONFIG_FILE; then
-	sed -i -e 's/^\([[:space:]]*\)AuthorizedKeysCommandRunAs\([[:space:]]\)/\1AuthorizedKeysCommandUser\2/i' $CONFIG_FILE
+        sed -i -e 's/^\([[:space:]]*\)AuthorizedKeysCommandRunAs\([[:space:]]\)/\1AuthorizedKeysCommandUser\2/i' $CONFIG_FILE
     else
         echo 'AuthorizedKeysCommandUser %u' >> $CONFIG_FILE
     fi
