07db1131603c1fd2d86afcfab2177aa955c7351a
jmazanek <jmazanek@redhat.com>
2018-04-25 22:41:07 +0200

    semanage_export fix syntax
     - updating syntax of semanage_export file to
       conform to the RHEL 7 semanage syntax

--- RHEL6_7/selinux/RestoreConfig/postupgrade.d/restoreConfig.sh
+++ RHEL6_7/selinux/RestoreConfig/postupgrade.d/restoreConfig.sh
@@ -2,7 +2,26 @@
 
 SEMANAGE_EXPORT_FILE=semanage_export
 
+semanage_syntax_update() {
+
+    ftype_opts=( '-f' '--ftype')
+    bool_opts=( '-1' '-0' '--on' '--off' )
+    local file=$1
+
+    for opt in "${bool_opts[@]}"
+    do
+        sed -i -r 's/'"$opt"'/-m '"$opt"'/g' "$file"
+    done
+
+    for ftype_opt in "${ftype_opts[@]}"
+    do
+        sed -i -r 's/'"$ftype_opt"'\s+directory/'"$ftype_opt"' d/g' "$file"
+    done
+}
+
+
 if test -s "$SEMANAGE_EXPORT_FILE"; then
+    semanage_syntax_update "$SEMANAGE_EXPORT_FILE"
     while read -r line
     do
         # restore saved configuration line by line
